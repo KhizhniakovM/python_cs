317f46335b8e9d5032d4d928c10379af8135eafb
Maksim Khizhniakov <khizhniakov@yandex-team.ru>
2021-11-12 15:02:34 +0300

    nums, booleans and strings

---
+++ basic/types.py
@@ -0,0 +1,22 @@
+# Types of numbers
+integer = 100
+floating = 100.1
+complex_number = complex(10, 20)
+
+# print(integer, floating, complex_number)
+
+#  Booleans
+boolean = True
+
+# Strings
+st = "string"
+print(len(st))
+print(st[0], st[1], st[2])
+print(st[-1], st[-2])
+
+# String slicing
+# string[start:end]
+# string[start:end:step]
+# string[end:start:-step]
+print(st[2:5:2])
+print(st[::-1])
