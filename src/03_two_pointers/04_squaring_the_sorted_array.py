c7aa77b777a6f5e5c30ac864c31f947b5ff3e720
Khizhniakov Max <khizhniakov@yandex-team.ru>
2022-04-23 10:46:11 +0300

    Complete two_pointers chapter

---
+++ src/03_two_pointers/04_squaring_the_sorted_array.py
@@ -0,0 +1,58 @@
+# Squaring a Sorted Array (easy)
+
+# * MARK: - Problem statement
+# Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+
+# * MARK: - Example 1
+# Input: [-2, -1, 0, 2, 3]
+# Output: [0, 1, 4, 4, 9]
+
+# * MARK: - Example 2
+# Input: [-3, -1, 0, 1, 2]
+# Output: [0, 1, 1, 4, 9]
+
+# * MARK: - Solution
+# This is a straightforward question. The only trick is that we can have negative numbers in the input array,
+# which will make it a bit difficult to generate the output array with squares in sorted order.
+
+# An easier approach could be to first find the index of the first non-negative number in the array.
+# After that, we can use Two Pointers to iterate the array.
+# One pointer will move forward to iterate the non-negative numbers,
+# and the other pointer will move backward to iterate the negative numbers.
+# At any step, whichever number gives us a bigger square will be added to the output array.
+# For the above-mentioned Example-1, we will do something like this:
+
+# images/03_two_pointers/004.png
+
+# Since the numbers at both ends can give us the largest square, an alternate approach could be to use
+# two pointers starting at both ends of the input array.
+# At any step, whichever pointer gives us the bigger square,
+# we add it to the result array and move to the next/previous number according to the pointer.
+# For the above-mentioned Example-1, we will do something like this:
+
+# images/03_two_pointers/005.png
+
+
+def make_squares(arr):
+    n = len(arr)
+    squares = [0 for x in range(n)]
+    highestSquareIdx = n - 1
+    left, right = 0, n - 1
+    while left <= right:
+        leftSquare = arr[left] * arr[left]
+        rightSquare = arr[right] * arr[right]
+        if leftSquare > rightSquare:
+            squares[highestSquareIdx] = leftSquare
+            left += 1
+        else:
+            squares[highestSquareIdx] = rightSquare
+            right -= 1
+        highestSquareIdx -= 1
+
+    return squares
+
+# * MARK: - Time Complexity
+# The above algorithm’s time complexity will be O(N) as we are iterating the input array only once.
+
+# * MARK: - Space Complexity
+# The above algorithm’s space complexity will also be O(N); this space will be used for the output array.
