79ada73621d5fd5c5b571d8372e1d7f4ab262018
Khizhniakov Max <khizhniakov@yandex-team.ru>
2022-05-18 12:34:06 +0300

    Complete tbfs chapter

---
+++ patterns_for_coding_questions/08_tree_breadth_first_search/07_level_order_successor.py
@@ -0,0 +1,85 @@
+# Level Order Successor (easy)
+
+# * MARK: - Problem statement
+# Given a binary tree and a node, find the level order successor of the given node in the tree.
+# The level order successor is the node that appears right after the given node in the level order traversal.
+
+# * MARK: - Example
+# images/08_tree_breadth_first_search/013.png
+# images/08_tree_breadth_first_search/014.png
+
+# * MARK: - Solution
+# This problem follows the Binary Tree Level Order Traversal pattern.
+# We can follow the same BFS approach. The only difference will be that we will not keep
+# track of all the levels. Instead we will keep inserting child nodes to the queue.
+# As soon as we find the given node, we will return the next node from the queue as the level order successor.
+
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def find_successor(root, key):
+    if root is None:
+        return None
+
+    queue = deque()
+    queue.append(root)
+    while queue:
+        currentNode = queue.popleft()
+        # insert the children of current node in the queue
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
+
+        # break if we have found the key
+        if currentNode.val == key:
+            break
+
+    return queue[0] if queue else None
+
+
+def main():
+
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+
+    result = find_successor(root, 3)
+    if result:
+        print(result.val)
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+
+    result = find_successor(root, 9)
+    if result:
+        print(result.val)
+
+    result = find_successor(root, 12)
+    if result:
+        print(result.val)
+
+
+main()
+
+# * MARK: - Time Complexity
+# The time complexity of the above algorithm is O(N),
+# where ‘N’ is the total number of nodes in the tree.
+# This is due to the fact that we traverse each node once.
+
+# * MARK: - Space Complexity
+# The space complexity of the above algorithm will be O(N) which is required for the queue.
+# Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level),
+# therefore we will need O(N) space to store them in the queue.
---
+++ patterns_for_coding_questions/08_tree_breadth_first_search/09_problem_challenge_tbfs_1.py
@@ -0,0 +1,76 @@
+# Problem Challenge 1
+# Connect All Level Order Siblings (medium)
+
+# * MARK: - Problem statement
+# Given a binary tree, connect each node with its level order successor.
+# The last node of each level should point to the first node of the next level.
+
+# * MARK: - Example
+# images/08_tree_breadth_first_search/016.png
+
+# * MARK: - Solution
+# This problem follows the Binary Tree Level Order Traversal pattern.
+# We can follow the same BFS approach. The only difference will be that while traversing we will
+# remember (irrespective of the level) the previous node to connect it with the current node.
+
+from __future__ import print_function
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right, self.next = None, None, None
+
+    # tree traversal using 'next' pointer
+    def print_tree(self):
+        print("Traversal using 'next' pointer: ", end='')
+        current = self
+        while current:
+            print(str(current.val) + " ", end='')
+            current = current.next
+
+
+def connect_all_siblings(root):
+    if root is None:
+        return
+
+    queue = deque()
+    queue.append(root)
+    currentNode, previousNode = None, None
+    while queue:
+        currentNode = queue.popleft()
+        if previousNode:
+            previousNode.next = currentNode
+        previousNode = currentNode
+
+        # insert the children of current node in the queue
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    connect_all_siblings(root)
+    root.print_tree()
+
+
+main()
+
+
+# * MARK: - Time Complexity
+# The time complexity of the above algorithm is O(N),
+# where ‘N’ is the total number of nodes in the tree.
+# This is due to the fact that we traverse each node once.
+
+# * MARK: - Space Complexity
+# The space complexity of the above algorithm will be O(N) which is required for the queue.
+# Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level),
+# therefore we will need O(N) space to store them in the queue.
