ad6efbb2a6e205e1da9e2598c3dd51c3a0c56731
Khizhniakov Max <khizhniakov@yandex-team.ru>
2022-06-17 13:16:39 +0300

    Add some chapters

---
+++ system_design_interview/01_system_design_problems/09_typeahead_suggestion.py
@@ -0,0 +1,7 @@
+# Designing Typeahead Suggestion
+# Let's design a real-time suggestion service, which will recommend terms to users as
+# they enter text for searching.
+
+# Similar Services: Auto-suggestions, Typeahead search
+# Difficulty: Medium
+
