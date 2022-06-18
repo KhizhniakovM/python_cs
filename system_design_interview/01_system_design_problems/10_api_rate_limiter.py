9517c98d4c2ac3cf9aeeab587b6d5792f4ca72bb
Khizhniakov Max <khizhniakov@yandex-team.ru>
2022-06-18 14:39:33 +0300

    Add some chapters

---
+++ system_design_interview/01_system_design_problems/10_api_rate_limiter.py
@@ -0,0 +1,6 @@
+# Designing an API Rate Limiter
+# Let's design an API Rate Limiter which will throttle users based upon the number of 
+# the requests they are sending.
+
+# Difficulty Level: Medium
+
