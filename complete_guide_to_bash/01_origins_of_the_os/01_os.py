33be106a410c61babf88d98ebf40fa67dca23070
Khizhniakov Max <khizhniakov@yandex-team.ru>
2022-06-26 12:31:00 +0300

    Add new theme: BASH

---
+++ complete_guide_to_bash/01_origins_of_the_os/01_os.py
@@ -0,0 +1,24 @@
+# Operating Systems
+# Learn why the operating system is an essential part of computers.
+
+# What is an OS?
+# Most modern computers require an operating system (OS), which manages computer processes, to work.
+# Before we launch a new application, we usually check its system requirements. These requirements specify
+# the hardware and OS that we need to launch the application.
+
+
+# Windows, Linux, and Mac operating systems
+# The system requirements tell us that the OS is a software platform, and the application requires it to
+# work correctly. But where did this requirement come from? Why can’t we just buy a computer and launch an
+# application without an OS?
+
+# Let’s consider these questions. Modern operating systems are multipurpose, so they offer users many features.
+# An OS provides users with the tools to interact with computer hardware. We can’t do too much or use much
+# computational power without them. So, an OS is required for any complex computations.
+
+# What is the downside of an OS? We may not use all of these features. However, we cannot disable them.
+# The OS utilizes the computer’s resources intensively to maintain all of its features. As a result, we have
+# much fewer computation resources for our applications. This overhead sometimes leads to a computer’s sluggish
+# performance, with programs hanging, or even the whole system rebooting.
+
+# To answer the question of why we need an OS, we’ll first examine the history of computers in the next lesson.
---
+++ complete_guide_to_bash/01_origins_of_the_os/02_electromechanical_computer.py
@@ -0,0 +1,34 @@
+# Electromechanical Computer
+# Learn what electromechanical computers are and how they work.
+
+# Let’s turn to history to discover the origins of operating systems. The first commercial OS was the GM-NAA I/O.
+# It appeared for the IBM 704 computer in 1956. All computers before then worked without any OS because they
+# didn’t have the high computational speed that requires an OS.
+
+# * MARK: - Tabulators
+# Let’s consider the first electromechanical computer. Herman Hollerith constructed it in 1890 and called it
+# the tabulator. The tabulator does not require an OS and a program, in the modern sense. It only performs a
+# limited set of arithmetic operations, and its hardware design defines them.
+
+# The tabulator loads input data for computation from punched cards. These cards look like sheets of thick paper
+# with holes punched in them. It works in the following steps:
+
+# 1. A human operator prepares punched cards and manually stacks them in special receivers.
+# 2. The operator threads the cards on the needles in the receivers.
+# 3. A short circuit occurs in each punched hole. Each short circuit increases the mechanical counter, which is a
+#   rotating cylinder.
+# 4. The tabulator displays calculation results on dials that resemble watches.
+
+# * MARK: - Why tabulators work slowly
+# By modern standards, the tabulator works very slowly. There are two reasons for this:
+
+# 1. It requires a lot of manual work for service. At the start, we need to prepare the input data. There is no way
+#   to punch the cards automatically. Then, we manually load the punched cards into the receivers. Both these
+#   operations require significant effort and time.
+
+# 2. The tabulator uses mechanical parts for computations. These parts are the needles to read data, rotating
+#   cylinders as counters, and dials to output the result. All these mechanics work slowly. It takes about one
+#   second to perform one elementary operation. No automation can accelerate it.
+
+# If we look at how the tabulator works, we find no place for an OS in it. The OS just does not have any
+# tasks to do on such a computer.
